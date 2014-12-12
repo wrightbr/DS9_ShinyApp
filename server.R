2c3afc0e1de4da36452b9f0637a6a294c86b3ac0
wrightbr <bw1630@gmail.com>
2014-12-12 05:37:02 -0800

    Fix mistake in sample size formula

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ shinyServer(function(input, output) {
     output$sampleSizeFpc <- renderPrint({
         
         numerator <- input$N * input$p * (1 - input$p)
-        denominator <- ((input$N - 1) * (input$d^2 / qnorm(input$alpha)^2)) + (input$p * (1 - input$p))
+        denominator <- ((input$N - 1) * (input$d^2 / qnorm((input$alpha)/2)^2)) + (input$p * (1 - input$p))
         round(numerator / denominator, 0)
         
     })
